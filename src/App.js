dad543ad1f2b45cafd4ad69654e3e14be245b23a
Marcell Benevento <marcellbenevento@gmail.com>
2024-07-08 19:48:30 -0400

    Initial completion of post listing page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
-//import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import PostListings from './pages/PostListings';
+import SuggestListing from './pages/SuggestListing';
 
 function App() {
   return (
     <div className="App flex flex-col min-h-screen">
-      <Header />
-      <div className="flex-grow">
-      </div>
-      <Footer />
+      <Router>
+        <Header />
+        <div className="flex-grow">
+          <Routes>
+            <Route path="/postlistings" element={<PostListings />} />
+            <Route path="/suggestlisting" element={<SuggestListing />} />
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
     </div>
   );
 }
