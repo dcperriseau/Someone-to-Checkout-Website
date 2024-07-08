1e8910a0804245c73c3b6072cc38dea8d7f4ba5f
Dominique Perriseau <domperriseau6@gmail.com>
2024-07-08 12:10:48 -0700

    added all fonts, background, and text colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,22 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#2d6064',
+        primary: '#2d6064', //white background color
+        secondary: '#f9f9f9', // light grey background color
+        accent: '#ebf9fa', //light blue accent background color 
+        pinkHeartColor: '#d24779', //pink like color
+        greyHeartColor: '#bababa', // grey no like color 
+        getPropertyLinkButton: '#47cad2', //get property verified button fill blue color 
+        getStartedButtonColor: '#47cad2', //get started buttobn fill blue color 
+        instagramIcon: '#d24779', //pink instagram logo color
+        linkedinIcon: '#076ae7', //blue LinkedIn logo color
+        twitterIcon: '#47cad2', //blue twitter logo color 
+        textPrimary: '#030303', // black text color
+        textSecondary: '#737373', //light grey text color
+        textTeritary: '#212121', // price text color, near black color
+        blueTextColor: '#47cad2', //turquoise font color
+        webTitleFontColor: '#2d6064' //Dark blue/indago website header font color
+
       },
       fontFamily: {
         'abril-fatface': ['"Abril Fatface"', 'cursive'],
