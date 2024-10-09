309977ca21de37c527e3caba0b169a06dc2980c2
Gabe Jimenez <jimenezgabriel12@gmail.com>
2024-10-08 20:02:45 -0600

    Add list of websites to render button on

--- chrome-extension/content-script.js
+++ chrome-extension/content-script.js
@@ -1,12 +1,28 @@
 console.log('Dibby Content Script Loaded.');
 
+const allowedWebsites = [
+    'facebook.com',
+    'zillow.com',
+    'apartments.com',
+    'trulia.com',
+    'craigslist.org',
+    'furnishedfinder.com',
+    'homes.com',
+    'rent.com',
+    'realtor.com',
+    'streeteasy.com',
+    'redfin.com',
+    'hotpads.com',
+    'apartmentfinder.com'
+];
+
 // Check if the URL matches the condition
-if (window.location.href.includes('chakra-ui.com')) {
+if (allowedWebsites.some(website => window.location.href.includes(website))) {
     // Create a container for the floating button UI
     const container = document.createElement('div');
     container.id = 'dibby-floating-container';
     container.style.position = 'fixed';
-    container.style.top = '20px';
+    container.style.top = '80px';
     container.style.right = '20px';
     container.style.zIndex = '10000';
     container.style.padding = '10px';
@@ -21,7 +37,7 @@ if (window.location.href.includes('chakra-ui.com')) {
 
     // Set the inner HTML to the content of your popup
     container.innerHTML = `
-      <h3>Submit Property URL</h3>
+      <h3 style="font-size: 20px;">Submit Property URL</h3>
       <button id="submitButton" style="border: 1px solid black; padding: 5px; margin-top: 10px; color: white; background-color: #b8b894; border-radius: 15px">Submit Current URL</button>
       <div id="status" class="status" style="display: none; margin-top: 10px; padding: 10px; text-align: center; border: 1px solid #ddd; border-radius: 5px;"></div>
     `;
